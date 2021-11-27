2525a56feee8702a793604188cf101ad76390b9f
Francis Githae <francis.githae@student.moringaschool.com>
2021-11-27 18:54:26 +0300

    add order prototypes

--- js/main.js
+++ js/main.js
@@ -1,4 +1,5 @@
 // define variables
+
 let pizzaPrices = [
   { size: "large", price: 1000 },
   { size: "medium", price: 800 },
@@ -19,110 +20,152 @@ let toppingPrices = [
   { name: "Pineapples", price: { large: 60, medium: 50, small: 40 } },
 ];
 
-function Order(size, toppings, crust) {
+function Order(size, toppings, crust, quantity) {
   this.size = size;
   this.toppings = toppings;
   this.crust = crust;
+  this.quantity = quantity;
+  this.total;
 }
 
-//display price values
-$("#large-price").text(pizzaPrices[0].price);
-$("#medium-price").text(pizzaPrices[1].price);
-$("#small-price").text(pizzaPrices[2].price);
-
-//get toppings prices
-let getToppingsPrices = (size) => {
-  let toppingPricesPerSize = toppingPrices.map((item) => {
-    return item.price[size];
-  });
-  return toppingPricesPerSize;
+//method to update the size of pizza
+Order.prototype.updateSize = function (newSize) {
+  this.size = newSize;
+  return this.size;
+};
+//update toppings array
+Order.prototype.updateToppings = function (newToppings) {
+  this.toppings = newToppings;
+  return this.toppings;
+};
+Order.prototype.updateQuantity = function (newQuantity) {
+  this.quantity = newQuantity;
+  return this.quantity;
+};
+Order.prototype.updateCrust = function (newCrust) {
+  this.crust = newCrust;
+  return this.crust;
 };
 
-//get crust prices
-let getCrustPrices = (size) => {
-  let crustPricesPerSize = crustPrices.map((item) => {
-    return item.price[size];
-  });
-  return crustPricesPerSize;
+Order.prototype.getTotal = function () {
+  return this.total;
 };
-// display toppings prices
+let cart = [];
+let orderItem;
 
-let toggleToppingPrices = (checked) => {
-  let activeSize = checked[0].value;
-  let activeToppingPrices = getToppingsPrices(activeSize);
-  // console.log(activeToppingPrices);
-  let toppingsCheckboxes = $(".toppings-check");
-
-  // console.log(toppingsPricesSpans);
-  for (i = 0; i < toppingsCheckboxes.length; i++) {
-    toppingsId = toppingsCheckboxes[i].id;
-    priceSpanId = "#" + toppingsId + "-price";
-    $(priceSpanId).text(activeToppingPrices[i]);
-    // toppingsPrice=toppingsPrice[]
-    // toppingsCheckboxes[i].text = activeToppingPrices[i];
-  }
-};
-
-// display crust prices
-let toggleCrustPrices = (checked) => {
-  let activeSize = checked[0].value;
-  let activeCrustPrices = getCrustPrices(activeSize);
-  console.log(activeCrustPrices);
-  for(i=0;i<3;i++){
-    crustInputId=$("[name=crustradio]");
-    console.log(crustInputId);
-    console.log(crustInputId[i].id);
-    crustPriceSpanId="#" + crustInputId[i].id + "-price";
-    console.log(crustPriceSpanId);
-    $(crustPriceSpanId).text(activeCrustPrices[i]);
-  }
-};
-// set default toppings prices
-toggleToppingPrices($("[name=sizeradio]"));
-
-//set default crust prices
-toggleCrustPrices($("[name=sizeradio]"));
-//event listener for size toggle
-$("[name=sizeradio]").on("change", () => {
-  let activeSize = $("[name=sizeradio]:checked");
-  toggleToppingPrices(activeSize);
-  toggleCrustPrices(activeSize);
-});
-//get checkbox values
-const getCheckboxValues = (selectedCheckboxes) => {
-  let valueArray = [];
-  for (i = 0; i < selectedCheckboxes.length; i++) {
-    valueArray.push(selectedCheckboxes[i].value);
-  }
-  return valueArray;
-};
-
-//Increase the quantity
-$("#add").on("click",()=>{
-  let quantityInput=$("#qty")
-  let quantity=parseInt(quantityInput.val()) + 1;
-  quantityInput.val(quantity);
-  console.log(quantity);
-
-})
-//Reduce quantity
-
-
-
-//Receive form input
-
-$("#form-order").submit((e) => {
-  e.preventDefault();
-  //get toppings selection
-  let toppings = $(".toppings-check:checked");
-  let crust = $("[name=crustradio]:checked").val();
-  let size = $("[name=sizeradio]:checked").val();
-  toppingsSelection = getCheckboxValues(toppings);
-  console.log(toppingsSelection);
-  console.log(crust);
-  console.log(size);
-});
 //dom ready
 $(() => {
   $("header").height($(window).height());
+  //display price values
+  $("#large-price").text(pizzaPrices[0].price);
+  $("#medium-price").text(pizzaPrices[1].price);
+  $("#small-price").text(pizzaPrices[2].price);
+
+  //get toppings prices
+  let getToppingsPrices = (size) => {
+    let toppingPricesPerSize = toppingPrices.map((item) => {
+      return item.price[size];
+    });
+    return toppingPricesPerSize;
+  };
+
+  //get crust prices
+  let getCrustPrices = (size) => {
+    let crustPricesPerSize = crustPrices.map((item) => {
+      return item.price[size];
+    });
+    return crustPricesPerSize;
+  };
+  // display toppings prices
+
+  let toggleToppingPrices = (checked) => {
+    let activeSize = checked[0].value;
+    let activeToppingPrices = getToppingsPrices(activeSize);
+    // console.log(activeToppingPrices);
+    let toppingsCheckboxes = $(".toppings-check");
+
+    // console.log(toppingsPricesSpans);
+    for (i = 0; i < toppingsCheckboxes.length; i++) {
+      toppingsId = toppingsCheckboxes[i].id;
+      priceSpanId = "#" + toppingsId + "-price";
+      $(priceSpanId).text(activeToppingPrices[i]);
+      // toppingsPrice=toppingsPrice[]
+      // toppingsCheckboxes[i].text = activeToppingPrices[i];
+    }
+  };
+
+  // display crust prices
+  let toggleCrustPrices = (checked) => {
+    let activeSize = checked[0].value;
+    let activeCrustPrices = getCrustPrices(activeSize);
+    console.log(activeCrustPrices);
+    for (i = 0; i < 3; i++) {
+      crustInputId = $("[name=crustradio]");
+      console.log(crustInputId);
+      console.log(crustInputId[i].id);
+      crustPriceSpanId = "#" + crustInputId[i].id + "-price";
+      console.log(crustPriceSpanId);
+      $(crustPriceSpanId).text(activeCrustPrices[i]);
+    }
+  };
+  // set default toppings prices
+  toggleToppingPrices($("[name=sizeradio]"));
+
+  //set default crust prices
+  toggleCrustPrices($("[name=sizeradio]"));
+  //event listener for size toggle
+  $("[name=sizeradio]").on("change", () => {
+    let activeSize = $("[name=sizeradio]:checked");
+    toggleToppingPrices(activeSize);
+    toggleCrustPrices(activeSize);
+  });
+  //get checkbox values
+  const getCheckboxValues = (selectedCheckboxes) => {
+    let valueArray = [];
+    for (i = 0; i < selectedCheckboxes.length; i++) {
+      valueArray.push(selectedCheckboxes[i].value);
+    }
+    return valueArray;
+  };
+
+  //Increase the quantity
+  $("#add").on("click", () => {
+    let quantityInput = $("#qty");
+    let quantity = parseInt(quantityInput.val()) + 1;
+    quantityInput.val(quantity);
+    console.log(quantity);
+  });
+  //Reduce quantity
+
+  $("#minus").on("click", () => {
+    let quantityInput = $("#qty");
+    let quantity = parseInt(quantityInput.val());
+    if (quantity > 0) {
+      quantity -= 1;
+      quantityInput.val(quantity);
+      console.log(quantity);
+    } else {
+      return;
+    }
+  });
+
+  //Receive form input
+
+  $("#form-order").submit((e) => {
+    e.preventDefault();
+    //get toppings selection
+    let toppings = $(".toppings-check:checked");
+    let crust = $("[name=crustradio]:checked").val();
+    let size = $("[name=sizeradio]:checked").val();
+    toppingsSelection = getCheckboxValues(toppings);
+    console.log(toppingsSelection);
+    console.log(crust);
+    console.log(size);
+  });
+  let orderItemCount = 0;
+  $("#order-now").on("click", () => {
+    orderItemCount += 1;
+    orderItem = new Order("large", [], "crispy", 1);
+    console.log(orderItem);
+  });
 });
