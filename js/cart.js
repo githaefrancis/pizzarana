b69ca613d1d41b02ae00ae50f86514aac3dd0151
Francis Githae <francis.githae@student.moringaschool.com>
2021-11-28 12:20:09 +0300

    add feature to display order details

--- js/cart.js
+++ js/cart.js
@@ -1,20 +1,21 @@
-$(()=>{
-
-  $('header').height($(document).height());
+$(() => {
+  // $('header').height($(document).height());
   // console.log(cart);
-  const shoppingCart=JSON.parse(localStorage.getItem("cart"));
+  const shoppingCart = JSON.parse(localStorage.getItem("cart"));
   // const shoppingCart=localStorage.getItem("cart");
-  $('#items-count').text(shoppingCart.length);
+  $("#items-count").text(shoppingCart.length);
   console.log(shoppingCart);
   console.log(shoppingCart.length);
   let singleCartItem;
-  for(i=0;i<shoppingCart.length;i++){
-    singleCartItem=`
+  for (i = 0; i < shoppingCart.length; i++) {
+    singleCartItem = `
     <div class="item row d-flex justify-content-start">
-                <div class="col-lg-2 mx-lg-5 col-sm-3" id="item-index">1</div>
+                <div class="col-lg-2 mx-lg-5 col-sm-3" id="item-index">${i}</div>
                 <div class="col-lg-5 col-sm-6">
                   <div class="title">
                     <p id="item-name">Pepperoni Pizza</p>
+                    <p>Size: <span class="badge bg-dark">large </span> Qty: <span class="badge bg-dark">1</span> Crust: <span class="badge bg-dark">Crispy</span></p>
+                    <p class="fw-bold"><em>Toppings</em></p>
                     <ul id="toppings-list">
                       <li>Mushrooms</li>
                       <li>Pineapples</li>
@@ -22,10 +23,10 @@ $(()=>{
                   </div>
                 </div>
                 <div class="col-lg-2 col-sm-12">
-                  <h3 class="" id="item-total">Ksh 1310</h3>
+                  <h2 class="badge bg-dark fs-4" id="item-total">Ksh 1310</h2>
                 </div>
               </div>
-    `
-    $('.cart-body').append(singleCartItem);
+    `;
+    $(".cart-body").append(singleCartItem);
   }
-})
+});
